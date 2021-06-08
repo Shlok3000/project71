0a2aad1d821359d3f7d68369ecd375cde29d3724
Shlok3000 <71341641+Shlok3000@users.noreply.github.com>
2021-06-08 18:06:00 -0400

    Add files via upload

---
+++ WriteStoryScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class WriteStoryScreen extends React.Component {
+    render(){
+        return(
+            <View>
+                <Text>Write some stories</Text>
+            </View>
+        )
+    }
+}
